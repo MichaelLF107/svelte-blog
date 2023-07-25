15158c3c887f2ac2e04fa5b3ca260520d5c96d30
Michael Friedrich <michaelfriedrich253@gmail.com>
2023-07-25 11:11:07 -0300

    Added cors to API

--- api/api.js
+++ api/api.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -11,6 +12,7 @@ const prisma = new PrismaClient()
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.post('/new-user', async (req, res) => {
     const { email, name, password } = req.body
